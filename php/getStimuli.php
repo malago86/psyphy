be220d7d349053acead7b95c60e9cc63ea529b9a
lago <lago@psych.ucsb.edu>
2020-06-09 10:49:18 -0700

    Composer update

--- php/getStimuli.php
+++ php/getStimuli.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
+require_once("vendor/autoload.php");
 require_once("google-api-php-client-2.4.1/vendor/autoload.php");
 
 
